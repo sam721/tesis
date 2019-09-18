956853f3460d974ad4476659861aa4e87f461fa6
Samuel Nacache Pérez <samuelnacache@gmail.com>
2019-09-17 20:08:10 -0400

    Updates

---
+++ precache-manifest.08cb8a30e7ca51427ef4fac2d7604603.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698709d4a693282daf8616f560180121",
+    "url": "/tesis/index.html"
+  },
+  {
+    "revision": "3d717fc33ac7f92b6397",
+    "url": "/tesis/static/css/2.3bf07905.chunk.css"
+  },
+  {
+    "revision": "d45dc7a63d2c0a1d29d6",
+    "url": "/tesis/static/css/main.95c318d4.chunk.css"
+  },
+  {
+    "revision": "3d717fc33ac7f92b6397",
+    "url": "/tesis/static/js/2.063b65c2.chunk.js"
+  },
+  {
+    "revision": "d45dc7a63d2c0a1d29d6",
+    "url": "/tesis/static/js/main.af2a2dd4.chunk.js"
+  },
+  {
+    "revision": "600eb75214f0225fc6d8",
+    "url": "/tesis/static/js/runtime~main.ca416940.js"
+  },
+  {
+    "revision": "79af0dfa0d1e19bfdfc3a58751b70a50",
+    "url": "/tesis/static/media/AVL.79af0dfa.png"
+  },
+  {
+    "revision": "674484b1c736d533d538001232ba331e",
+    "url": "/tesis/static/media/graph3.674484b1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tesis/precache-manifest.2a9061cbf3cebade7970e25abe3cae4e.js"
+  "/tesis/precache-manifest.08cb8a30e7ca51427ef4fac2d7604603.js"
 );
 
 self.addEventListener('message', (event) => {
