886ccbb3e48957a2a8d436a343cccd026bd837b6
unknown <samuelnacache@gmail.com>
2019-10-11 23:32:32 -0300

    Added mobile warning

--- tesis/src/App.tsx
+++ tesis/src/App.tsx
@@ -9,8 +9,46 @@ import CodeBlock from './Components/CodeBlock';
 import Footer from './Components/Footer';
 import MenuBar from './Components/MenuBar';
 import FreeGraphModal from './Components/FreeGraphModal';
-class App extends React.Component<{ algorithm: string, show: boolean }>{
+type Props = {
+  algorithm: string, show: boolean 
+} 
+type State = {
+  isMobile: boolean,
+}
+class App extends React.Component<Props, State>{
+  constructor(props: Props){
+    super(props);
+    this.state = { isMobile: window.innerWidth <= 800}
+  }
+
+  handleWindowResize = () => {
+    this.setState({isMobile: window.innerWidth <= 800});
+  }
+
+  componentWillMount(){
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
   render() {
+    const {isMobile} = this.state;
+    if(isMobile){
+      return (
+        <Container>
+          <Row>
+            <Col>
+              <div className="mobile home-title">
+                <p>¡Ups! :(</p>
+                <p>Esta aplicación esta pensada para ser visualizada en dispositivos de mayor resolución</p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
     return (
       <div>
         <Container>
